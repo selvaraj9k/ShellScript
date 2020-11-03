30ffb861964ae8ca8c96b0b8d7572473a5d9f5c8
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-11-03 23:40:08 +0530

    Special Variables

---
+++ SpecialVaribles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Special Variables
+echo "File Name of current script = $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values : $@"
+echo "Quoted Values : $*"
+echo "Total Number of Parameters : $#"
+echo "Process Id number : $0"
+echo "PID of the last background command :$!"
+echo "Exit status for last command : $?"
