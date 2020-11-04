f4b7210238cdc7284e81cb12b76f44f57af9414e
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-11-04 20:07:02 +0530

    Arithematic Operator

---
+++ Arthematic_Operator_3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#Arithematic operators
+
+echo "Enter the Number : "
+read a
+echo "Enter the NUmber : "
+read b
+if [ $a = $b ]
+then
+        echo "True"
+else
+        echo "False"
+fi
+
