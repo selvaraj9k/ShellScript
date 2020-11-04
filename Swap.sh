0069e2569aa991614ada2e97803db9828f16da10
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-11-04 12:22:58 +0530

    Swapping of Two Numbers

---
+++ Swap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Swappping of Two Numbers
+echo "Enter the Number : "
+read NO_1
+echo "Enter the Number : "
+read NO_2
+echo "BEFORE SWAPPING"
+echo "NO_1=$NO_1"
+echo "NO_2=$NO_2"
+TEMP=$NO_1
+NO_1=$NO_2
+NO_2=$TEMP
+echo "AFTER SWAPPING"
+echo "NO_1=$NO_1"
+echo "NO_2=$NO_2"
