19e60f5b4ce36b1a0345613a4a52883eaa70cc64
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-11-12 08:03:05 +0530

    escape sequences

---
+++ escape_sequences.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Linux is open source\a\f"
+echo "ubuntuis best operting system for beginner"
+echo "kali\clinux"
+echo "linus torvalds"
+echo "UNIX linux systems"
+echo "windows\tlinux\nunix\tandroid\t"
+echo "apache\vtomcat\vnginix\v"
