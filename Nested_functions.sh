0f51fb8241d9c0f39302e939e9a09890fea5da3f
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-11-19 07:12:42 +0530

    Nested functions

---
+++ Nested_functions.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+name ()
+{
+	echo " I am selvaraj"
+	linux
+}
+
+linux ()
+{
+	echo " Currently i am learning linux"
+	ubuntu
+}
+
+ubuntu ()
+{
+	echo  " Ubuntu is a best operting systems for beginner"
+	centos
+}
+
+centos ()
+{
+	echo " Centos is based on Redhat linux"
+}
+name 
+
